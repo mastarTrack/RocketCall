2a8d918a7e71825ede2320660b968bc674edeae0
devBambu <byrr113@gmail.com>
2026-03-25 23:36:55 +0900

    🩹chore: 설명 주석 추가

--- RocketCall/View/HomeTab/HomeMainViewController.swift
+++ RocketCall/View/HomeTab/HomeMainViewController.swift
@@ -21,6 +21,6 @@ class HomeMainViewController: UIViewController {
         navigationController?.navigationBar.isHidden = true
         
         addChild(homeMainView.chartHostingController) // UIHostingVC와 현재 VC의 생명주기 동기화
-        homeMainView.chartHostingController.didMove(toParent: self)
+        homeMainView.chartHostingController.didMove(toParent: self) // 자식 VC(hostingVC)에게 VC 계층에 추가되었음을 알림
     }
 }
