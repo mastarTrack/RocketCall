819045d622a91f48cb743c283454cce1b02f6c3a
yeseul jang <yyssjj9999@gmail.com>
2026-03-25 19:53:54 +0900

    🩹chore:  ViewController에 불필요한 코드 제거

--- RocketCall/View/ViewController.swift
+++ RocketCall/View/ViewController.swift
@@ -10,36 +10,10 @@ import SnapKit
 
 // StateLabel 예시 및 NavigationController 타이틀 설정 예시
 class ViewController: UIViewController {
-    let test = MissionResultView()
-    let SuccessData = MissionResultData(
-        state: true,
-        missionName: "성공 미션",
-        route: "지구 → 달",
-        duration: "00:14",
-        type: "계획된 미션",
-        statusText: "완료",
-        completedDate: "2026. 3. 20. 오후 5:41:08"
-    )
-    
-    let failData = MissionResultData(
-        state: false,
-        missionName: "실패 미션",
-        route: "지구 → 화성",
-        duration: "00:08",
-        type: "즉시 미션",
-        statusText: "실패",
-        completedDate: "2026. 3. 25. 오후 9:10:00"
-    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
-        
-        view.addSubview(test)
-        test.snp.makeConstraints {
-            $0.edges.equalTo(view.safeAreaLayoutGuide)
-        }
-        test.configure(with: SuccessData)
     }
 }
 
