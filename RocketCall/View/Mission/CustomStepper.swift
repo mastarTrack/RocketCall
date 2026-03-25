b2b63bd27a210d80f131bf50d0fcba323319a3a9
Bin <ybin4548@gmail.com>
2026-03-25 12:34:24 +0900

    ✨feat: CustomStepper 생성

---
+++ RocketCall/View/Mission/CustomStepper.swift
@@ -0,0 +1,118 @@
+//
+//  CustomStepper.swift
+//  RocketCall
+//
+//  Created by 손영빈 on 3/25/26.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class CustomStepper: UIView {
+    
+    private let disposeBag = DisposeBag()
+    let value: BehaviorRelay<Int>
+    
+    private let labelStackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    
+    private let stepperStackView = UIStackView()
+    private let plusButton = CircleButton(size: 30, backgroundColor: .cardBackground, image: UIImage(systemName: "plus"), tintColor: .mainLabel)
+    private let minusButton = CircleButton(size: 30, backgroundColor: .cardBackground, image: UIImage(systemName: "minus"), tintColor: .mainLabel)
+    private let valueLabel = UILabel()
+    
+    init(title: String, subtitle: String, initialValue: Int) {
+        self.value = BehaviorRelay(value: initialValue)
+        super.init(frame: .zero)
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        setAttributes()
+        setLayout()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CustomStepper {
+    private func bind() {
+        value
+            .map { "\($0)" }
+            .bind(to: valueLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        value
+            .map { $0 > 1 }
+            .bind(to: minusButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        value
+            .map { $0 < 180 }
+            .bind(to: plusButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        plusButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self else { return }
+                self.value.accept(self.value.value + 1)
+            }).disposed(by: disposeBag)
+        
+        minusButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self else { return }
+                self.value.accept(self.value.value - 1)
+            }).disposed(by: disposeBag)
+
+    }
+}
+
+extension CustomStepper {
+    private func setAttributes() {
+        let titleConfig = LabelConfiguration.title
+        let subTitleConfig = LabelConfiguration.subTitle
+        
+        titleLabel.font = titleConfig.font
+        titleLabel.textColor = titleConfig.color
+        
+        subtitleLabel.font = subTitleConfig.font
+        subtitleLabel.textColor = subTitleConfig.color
+        
+        valueLabel.font = titleConfig.font
+        valueLabel.textColor = titleConfig.color
+        valueLabel.textAlignment = .center
+        valueLabel.setContentHuggingPriority(.required, for: .horizontal)
+        
+        labelStackView.axis = .vertical
+        labelStackView.spacing = 4
+        labelStackView.alignment = .leading
+        
+        stepperStackView.axis = .horizontal
+        stepperStackView.spacing = 14
+        stepperStackView.alignment = .center
+    }
+    
+    private func setLayout() {
+        [titleLabel, subtitleLabel].forEach { labelStackView.addArrangedSubview($0) }
+        [minusButton, valueLabel, plusButton].forEach { stepperStackView.addArrangedSubview($0) }
+        
+        addSubview(labelStackView)
+        addSubview(stepperStackView)
+        
+        labelStackView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.bottom.equalToSuperview().inset(10)
+            $0.trailing.lessThanOrEqualTo(stepperStackView.snp.leading).offset(-12)
+        }
+        
+        stepperStackView.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.top.bottom.equalToSuperview().inset(10)
+        }
+    }
+}
+
