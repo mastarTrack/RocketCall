9785c8e1aa34cdb99384ba01fb5e29954188ca67
devBambu <byrr113@gmail.com>
2026-03-27 15:00:49 +0900

    ✨feat: DetailCollectionView 생성

---
+++ RocketCall/View/HomeTab/View/DetailCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCollectionView.swift
+//  RocketCall
+//
+//  Created by t2025-m0143 on 3/27/26.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailCollectionView: UICollectionView {
+    enum Section: Int {
+        case sum = 0
+        case chart
+        case progress
+        case result
+    }
+    
+
+}
